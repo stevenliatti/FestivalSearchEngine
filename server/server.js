04c6052fc13582fb64bc33165ef4f6777cf368c8
steven.liatti <steven.liatti@etu.hesge.ch>
2017-11-22 14:09:18 +0100

    Continue doc and add apidoc.json. Run `apidoc -e node_modules` in server/ to generate doc
    On branch master
    
    Changes to be committed:
    	new file:   server/apidoc.json
    	modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -11,9 +11,7 @@ const key_eventful = "WSpR5fzQ69N3w2FL";
 const url_eventful = "http://api.eventful.com/json/events/search?app_key=" + key_eventful + 
 "&location=Switzerland&category=music,festivals_parades&page_size=100&date=future&sort_order";
 
-/**
- * "Demo"
- */
+
 app.get('/events', function(req, res) {
 	let events = [];
 	request(url_eventful, function (error, response, body) {
@@ -46,7 +44,6 @@ app.get('/events', function(req, res) {
  * @apiGroup events
  * @apiVersion  0.1.0
  * 
- * 
  * @apiParam  {String} artist Optional artist name
  * @apiParam  {String} location Optional location name
  * 
@@ -61,10 +58,14 @@ app.get('/events', function(req, res) {
  * @apiSuccess {String}   events.postal_code Event's postal code
  * @apiSuccess {String}   events.latitude Event's latitude
  * @apiSuccess {String}   events.longitude Event's longitude
+ * @apiSuccess {String}   events.offer Event's offer URL to buy ticket for example
  * 
  * @apiSuccess {Object[]} events.performers Event's performers
  * @apiSuccess {String}   events.performers.name Performer's name
  * @apiSuccess {String}   events.performers.music_kind Performer's kind of music (short)
+ * @apiSuccess {String}   events.performers.thumb Little image URL of the artist
+ * @apiSuccess {String}   events.performers.image Image URL of the artist
+ * @apiSuccess {String}   events.performers.facebook Facebook URL of the artist
  * 
  * 
  * @apiParamExample  {json} Request-Example:
@@ -75,7 +76,6 @@ app.get('/events', function(req, res) {
  * 
  * 
  * @apiSuccessExample {json} Success-Response:
- * HTTP/1.1 200 OK
  * {
  *   "events": [
  *     {
@@ -89,18 +89,19 @@ app.get('/events', function(req, res) {
  *       "postal_code": "CH-1218",
  *       "latitude": "46.2",
  *       "longitude": "6.16667",
+ *       "offer": "https://www.bandsintown.com/t/17938982?app_id=hehe&came_from=267",
  *       "performers": [
  *         {
  *           "name": "Metallica",
- *           "music_kind": "Metal / Rock"
+ *           "music_kind": "Metal / Rock",
+ *           "thumb": "https://s3.amazonaws.com/bit-photos/thumb/6874519.jpeg",
+ *           "image": "https://s3.amazonaws.com/bit-photos/large/6874519.jpeg",
+ *           "facebook": ""
  *         }
  *       ]
  *     }
  *   ]
  * }
- * 
- * @apiError
- * 
  */
 app.get('/events/artist=:artist?/location=:location?', function(req, res) {
 	console.log(req.params);
@@ -115,6 +116,45 @@ app.get('/events/artist=:artist?/location=:location?', function(req, res) {
 	}
 });
 
+/**
+ * 
+ * @api {get} /infos/artist=:artist Return infos of the artist
+ * @apiName GetInfos
+ * @apiGroup infos
+ * @apiVersion  0.1.0
+ * 
+ * 
+ * @apiParam  {String} artist Artist name
+ * 
+ * @apiSuccess {Object}  artist Artist object
+ * @apiSuccess {String}  artist.name Artist's name
+ * @apiSuccess {String}  artist.type Information to know if it's a band or single artist
+ * @apiSuccess {String}  artist.country Artist's country origin
+ * @apiSuccess {String}  artist.disambiguation Artist's disambiguation = kind of music
+ * @apiSuccess {Object}  artist.life-span Infos about life span of artist
+ * @apiSuccess {Boolean} artist.life-span.ended If the artist/group don't exist anymore
+ * @apiSuccess {String}  artist.life-span.begin Begin date of artist/group, format : "YYYY-MM-DD" or "YYYY-MM" or "YYYY"
+ * @apiSuccess {String}  artist.life-span.end End date of artist/group, format : "YYYY-MM-DD" or "YYYY-MM" or "YYYY"
+ * 
+ * @apiParamExample  {json} Request-Example:
+ * {
+ *   "artist": "Nirvana"
+ * }
+ * 
+ * @apiSuccessExample {json} Success-Response:
+ * {
+ *   "name": "Nirvana",
+ *   "type": "Group",
+ *   "country": "US",
+ *   "disambiguation": "90s US grunge band",
+ *   "life-span": {
+ *      "ended": true,
+ *      "begin": "1988-02",
+ *      "end": "1994-04-05"
+ *    },
+ * }
+ * 
+ */
 app.get('/infos/artist=:artist', function(req, res) {
 
 });
