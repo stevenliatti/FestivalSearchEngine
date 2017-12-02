a79f9b8300bc345b75ef1e0690fdad1126cc2210
steven.liatti <steven.liatti@etu.hesge.ch>
2017-12-02 19:31:38 +0100

    Change folder structure, now server is capable to serve static client pages Next : deploy to AWS ;-)
    On branch master
    
    Changes to be committed:
    	modified:   .gitignore
    	renamed:    server/.gitignore -> src/.gitignore
    	renamed:    server/consts.js -> src/backend/consts.js
    	renamed:    server/events.js -> src/backend/events.js
    	renamed:    server/infos.js -> src/backend/infos.js
    	renamed:    server/tracks.js -> src/backend/tracks.js
    	renamed:    server/use.js -> src/backend/use.js
    	renamed:    web/index.html -> src/frontend/index.html
    	new file:   src/frontend/routes.html
    	renamed:    web/style/BreeSerif_Reg.otf -> src/frontend/style/BreeSerif_Reg.otf
    	renamed:    web/style/main.css -> src/frontend/style/main.css
    	renamed:    server/package-lock.json -> src/package-lock.json
    	renamed:    server/package.json -> src/package.json
    	renamed:    server/server.js -> src/server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,29 @@
-const consts = require('./consts');
-const use = require('./use');
-const events = require('./events');
-const infos = require('./infos');
-const tracks = require('./tracks');
+const consts = require("./backend/consts");
+const use = require("./backend/use");
+const events = require("./backend/events");
+const infos = require("./backend/infos");
+const tracks = require("./backend/tracks");
+
+const path = require("path");
 
 app.use(cors());
+app.use("/frontend", express.static(path.join(__dirname, "frontend")));
+app.use("/doc", express.static(path.join(__dirname, "doc")));
+
+app.get("/", function(req, res) {
+    log.debug("index.html");
+    res.sendFile(path.join(__dirname, "frontend/index.html"));
+});
+
+app.get("/doc", function(req, res) {
+    log.debug("doc");
+    res.sendFile(path.join(__dirname, "doc/index.html"));
+});
+
+app.get("/routes", function(req, res) {
+    log.debug("routes");
+    res.sendFile(path.join(__dirname, "frontend/routes.html"));
+});
 
 /**
  * @api {get} /events/artist=:artist?/location=:location? 
@@ -21,22 +40,22 @@ app.use(cors());
  * @apiParam  {String} location Optional location name
  *
  * @apiSuccess {Object[]} events Array of events (it can be empty)
- * @apiSuccess {String}   events.id Event's id
- * @apiSuccess {String}   events.title Event's title
- * @apiSuccess {String}   events.venue Event's venue
- * @apiSuccess {String}   events.date Event's date, format : "YYYY-MM-DD hh:mm:ss"
- * @apiSuccess {String}   events.address Event's postal address
- * @apiSuccess {String}   events.city Event's city
- * @apiSuccess {String}   events.region Event's region
- * @apiSuccess {String}   events.postal_code Event's postal code
- * @apiSuccess {String}   events.country Event's country
- * @apiSuccess {String}   events.latitude Event's latitude
- * @apiSuccess {String}   events.longitude Event's longitude
- * @apiSuccess {String}   events.offer Event's offer URL to buy ticket for example
+ * @apiSuccess {String}   events.id Event"s id
+ * @apiSuccess {String}   events.title Event"s title
+ * @apiSuccess {String}   events.venue Event"s venue
+ * @apiSuccess {String}   events.date Event"s date, format : "YYYY-MM-DD hh:mm:ss"
+ * @apiSuccess {String}   events.address Event"s postal address
+ * @apiSuccess {String}   events.city Event"s city
+ * @apiSuccess {String}   events.region Event"s region
+ * @apiSuccess {String}   events.postal_code Event"s postal code
+ * @apiSuccess {String}   events.country Event"s country
+ * @apiSuccess {String}   events.latitude Event"s latitude
+ * @apiSuccess {String}   events.longitude Event"s longitude
+ * @apiSuccess {String}   events.offer Event"s offer URL to buy ticket for example
  *
- * @apiSuccess {Object[]} events.performers Event's performers
- * @apiSuccess {String}   events.performers.name Performer's name
- * @apiSuccess {String}   events.performers.short_bio Performer's kind of music (short)
+ * @apiSuccess {Object[]} events.performers Event"s performers
+ * @apiSuccess {String}   events.performers.name Performer"s name
+ * @apiSuccess {String}   events.performers.short_bio Performer"s kind of music (short)
  * @apiSuccess {String}   events.performers.thumb Little image URL of the artist
  * @apiSuccess {String}   events.performers.image Image URL of the artist
  * @apiSuccess {String}   events.performers.facebook Facebook URL of the artist
@@ -92,7 +111,7 @@ app.use(cors());
  *   "no_events": true
  * }
  */
-app.get('/events/artist=:artist?/location=:location?', events.events);
+app.get("/events/artist=:artist?/location=:location?", events.events);
 
  /**
  * @api {get} /infos/artist=:artist 
@@ -107,26 +126,26 @@ app.get('/events/artist=:artist?/location=:location?', events.events);
  * @apiParam  {String} artist Artist name
  *
  * @apiSuccess {Object}    artist Artist object
- * @apiSuccess {String}    artist.name Artist's name
- * @apiSuccess {String}    artist.description Artist's description (from wikipedia)
- * @apiSuccess {String}    artist.type Information to know if it's a band or single artist
- * @apiSuccess {String}    artist.country Artist's country origin
- * @apiSuccess {String}    artist.disambiguation Artist's disambiguation = kind of music
+ * @apiSuccess {String}    artist.name Artist"s name
+ * @apiSuccess {String}    artist.description Artist"s description (from wikipedia)
+ * @apiSuccess {String}    artist.type Information to know if it"s a band or single artist
+ * @apiSuccess {String}    artist.country Artist"s country origin
+ * @apiSuccess {String}    artist.disambiguation Artist"s disambiguation = kind of music
  * @apiSuccess {Object}    artist.life_span Infos about life span of artist
- * @apiSuccess {Boolean}   artist.life_span.ended If the artist/group don't exist anymore
+ * @apiSuccess {Boolean}   artist.life_span.ended If the artist/group don"t exist anymore
  * @apiSuccess {String}    artist.life_span.begin Begin date of artist/group, format : "YYYY-MM-DD" or "YYYY-MM" or "YYYY"
  * @apiSuccess {String}    artist.life_span.end End date of artist/group, format : "YYYY-MM-DD" or "YYYY-MM" or "YYYY"
  * @apiSuccess {String}    artist.image Image URL
  * @apiSuccess {String}    artist.thumb Thumb URL
  * @apiSuccess {String}    artist.facebook Facebook URL
  * @apiSuccess {String[]}  artist.genres Array of string containing genres from Spotify
- * @apiSuccess {String}    artist.id Spotify's id
+ * @apiSuccess {String}    artist.id Spotify"s id
  * @apiSuccess {Object[]}  artist.images Additionnal images from Spotify
  * @apiSuccess {Integer}   artist.images.height Height of image
  * @apiSuccess {Integer}   artist.images.width Width of image
  * @apiSuccess {String}    artist.images.url URL of image
  * 
- * @apiError artist_not_found {Boolean} The given artist wasn't found
+ * @apiError artist_not_found {Boolean} The given artist wasn"t found
  * @apiError no_artist_provided {Boolean} No artist was given to the API
  *
  * @apiParamExample  {json} Request-Example:
@@ -166,7 +185,7 @@ app.get('/events/artist=:artist?/location=:location?', events.events);
  *   "no_artist_provided": true
  * }
  */
-app.get('/infos/artist=:artist?', infos.infos);
+app.get("/infos/artist=:artist?", infos.infos);
 
 /**
  * @api {get} /tracks/artist=:artist/country_code=:country_code
@@ -176,20 +195,20 @@ app.get('/infos/artist=:artist?', infos.infos);
  * @apiVersion  0.1.0
  *
  * @apiDescription Return the top-tracks infos for this artist and country code from Spotify.
- * Also contains the track preview's URL if existent.
+ * Also contains the track preview"s URL if existent.
  * To note that all string fields can be string empty ("").
  * 
  * @apiParam  {String} artist The artist name
- * @apiParam  {String} country_code The country's code (ISO 3166-1 alpha-2)
+ * @apiParam  {String} country_code The country"s code (ISO 3166-1 alpha-2)
  *
  * @apiSuccess {Object[]} tracks Array of tracks
- * @apiSuccess {String}   tracks.id Tracks's id
- * @apiSuccess {String}   tracks.name Tracks's title
- * @apiSuccess {String}   tracks.album Name of the album the track's in
+ * @apiSuccess {String}   tracks.id Tracks"s id
+ * @apiSuccess {String}   tracks.name Tracks"s title
+ * @apiSuccess {String}   tracks.album Name of the album the track"s in
  * @apiSuccess {String}   tracks.preview_url URL of the track preview
- * @apiSuccess {Number}   tracks.popularity Spotify's popularity rank
+ * @apiSuccess {Number}   tracks.popularity Spotify"s popularity rank
  *
- * @apiError artist_not_found {Boolean} The given artist wasn't found
+ * @apiError artist_not_found {Boolean} The given artist wasn"t found
  * @apiError no_artist_and_code_provided {Boolean} No artist and code was given to the API 
  * @apiError preview_not_found {Boolean} No top tracks has preview
  * 
@@ -231,6 +250,11 @@ app.get('/infos/artist=:artist?', infos.infos);
  *   "preview_not_found": true
  * }
  */
-app.get('/tracks/artist=:artist?/country_code=:country_code?', tracks.tracks);
+app.get("/tracks/artist=:artist?/country_code=:country_code?", tracks.tracks);
+
+app.all("*", function(req, res) {
+	log.error("Requested:", req.url);
+	res.status(404).send("404 Page not found.");
+});
 
 app.listen(8080);
